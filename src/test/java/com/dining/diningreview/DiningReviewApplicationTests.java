ce96e9e0314260540eb7ee13f8d9ad12d8f8afe2
Francisquet Vargas <fvs00725@gmail.com>
2022-11-08 10:28:36 -0600

    initial commit

---
+++ src/test/java/com/dining/diningreview/DiningReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dining.diningreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
